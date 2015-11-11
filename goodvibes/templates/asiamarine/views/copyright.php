767cd515a5edb36a9e30f95437fb5ed5a45026d0
jrdeguzman <jpaolo.deguzman@gmail.com>
2015-11-11 15:37:35 +0800

    replaced files

--- goodvibes/templates/asiamarine/views/copyright.php
+++
@@ -1,6 +0,0 @@
-<div id="copy-line" class="clr">
-<div id="copy-structure">
-<div id="copy">Copyright &copy; <?php echo date("Y"); ?> <?php if(empty($selectcopy)) { echo "$sitename"; } elseif($selectcopy) { echo "$selectcopy"; } ?>. All Right Reserved.</div>
-<div id="powered">Powered By <a href="http://www.joomlaperfect.com" target="_blank">Joomla Perfect</a></div>
-</div>
-</div>
--- goodvibes/templates/finedining/framework/banner.php
+++
@@ -1,8 +0,0 @@
-<?php if($bannerwidth=="fix") { ?><div id="spacer" class="clr">&nbsp;</div><?php } ?>
-<?php if($this->countModules('banner')) : ?>
-<div id="banner-outer" class="clr">
-<div<?php if($bannerwidth=="fix") { echo ' id="banner-inner"'; } ?>>
-<jdoc:include type="modules" name="banner" style="none" />
-</div>
-</div>
-<?php endif; ?>
